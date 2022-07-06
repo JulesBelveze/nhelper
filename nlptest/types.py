c6533f3d19dc494ed80ef089b7029b4d55947808
JulesBelveze <jules.belveze@hotmail.fr>
2022-07-06 07:26:22 +0200

    chore: add binary computation

--- nlptest/types.py
+++ nlptest/types.py
@@ -19,16 +19,15 @@ class TaskType(str, Enum):
     token_classification = "token_classification"
 
 
-class Span(BaseModel):
-    """Representation of a 'Span' object for span classification tasks"""
+class BinarySpan(BaseModel):
+    """Binary representation of a 'Span' object for span classification tasks"""
     start: int
     end: int
-    label: Union[str, int]
     text: str = None
     prob: float = None
 
     def __str__(self):
-        return str(self.start) + "_" + str(self.end) + "_" + str(self.label)
+        return str(self.start) + "_" + str(self.end)
 
     def __hash__(self):
         return hash(str(self))
@@ -38,8 +37,30 @@ class Span(BaseModel):
 
     def __eq__(self, other):
         if other.__class__ is self.__class__:
+            return str(self.start) + "_" + str(self.end) == \
+                   str(other.start) + "_" + str(other.end)
+
+
+class Span(BinarySpan):
+    """Representation of a 'Span' object for span classification tasks"""
+    label: Union[Union[str, int], List[Union[str, int]]]
+
+    def __str__(self):
+        return str(self.start) + "_" + str(self.end) + "_" + str(self.label)
+
+    def __eq__(self, other):
+        if other.__class__ is self.__class__:
             return str(self.start) + "_" + str(self.end) + "_" + str(self.label) == \
                    str(other.start) + "_" + str(other.end) + "_" + str(other.label)
+
+    def to_binary(self):
+        """"""
+        return BinarySpan(
+            start=self.start,
+            end=self.end,
+            text=self.text,
+            prob=self.prob
+        )
 
 
 class SpanClassificationOutput(BaseModel):
@@ -54,6 +75,15 @@ class SpanClassificationOutput(BaseModel):
             return False
 
         return sorted(self.y) == sorted(self.y_pred)
+
+    @property
+    def binary_success(self):
+        if len(self.y) != len(self.y_pred):
+            return False
+
+        binary_y = [span.to_binary() for span in self.y]
+        binary_y_pred = [span.to_binary() for span in self.y_pred]
+        return sorted(binary_y_pred) == sorted(binary_y)
 
 
 class SequenceClassificationOutput(BaseModel):
--- tests/test_performer.py
+++ tests/test_performer.py
@@ -30,10 +30,10 @@ class TestPerformer:
         performer.fit([seq_classification_behavior, seq_classification_behavior2])
         print(performer.result)
         assert performer.result[f"Behavior type - {BehaviorType.directional.value}"] == \
-               performer.result[f"Name - {seq_classification_behavior2.name}"] == 0.5
+               performer.result[f"Name - {seq_classification_behavior2.name}"] == [0.5, '1/2']
         assert performer.result[f"Behavior type - {BehaviorType.invariance.value}"] == \
-               performer.result[f"Name - {seq_classification_behavior.name}"] == 1.0
-        assert performer.result["Total"] == 2 / 3
+               performer.result[f"Name - {seq_classification_behavior.name}"] == [1.0, '1/1']
+        assert performer.result["Total"] == [2 / 3, '2/3']
 
     def test_metrics_span_classification(self):
         """"""
@@ -50,7 +50,10 @@ class TestPerformer:
             name="Test span classification 2",
             test_type=BehaviorType.directional,
             samples=["This is a test", "This is a 2nd test"],
-            labels=[[Span(start=0, end=8, label=0)], [Span(start=2, end=5, label=1), Span(start=1, end=8, label=0)]],
+            labels=[
+                [Span(start=0, end=8, label=0)],
+                [Span(start=2, end=5, label=1), Span(start=1, end=8, label=0)]
+            ],
             predict_fn=lambda x: [
                 [Span(start=0, end=4, label=1)],
                 [Span(start=1, end=2, label=0), Span(start=2, end=5, label=1), Span(start=4, end=6, label=1)]
@@ -62,10 +65,46 @@ class TestPerformer:
         performer.fit([span_classification_behavior, span_classification_behavior2])
 
         assert performer.result[f"Behavior type - {BehaviorType.directional.value}"] == \
-               performer.result[f"Name - {span_classification_behavior2.name}"] == 0.0
+               performer.result[f"Name - {span_classification_behavior2.name}"] == [0.0, '0/2']
         assert performer.result[f"Behavior type - {BehaviorType.invariance.value}"] == \
-               performer.result[f"Name - {span_classification_behavior.name}"] == 1.0
-        assert performer.result["Total"] == 1 / 3
+               performer.result[f"Name - {span_classification_behavior.name}"] == [1.0, '1/1']
+        assert performer.result["Total"] == [1 / 3, '1/3']
+
+    def test_metrics_span_classification_binarized(self):
+        """"""
+        span_classification_behavior = SpanClassificationBehavior(
+            capability="Capability 1",
+            name="Test span classification",
+            test_type=BehaviorType.invariance,
+            samples=["This is a test"],
+            labels=[[Span(start=0, end=10, label=4)]],
+            predict_fn=lambda x: [[Span(start=0, end=10, label=5)], ] * len(x)
+        )
+        span_classification_behavior2 = SpanClassificationBehavior(
+            capability="Capability 2",
+            name="Test span classification 2",
+            test_type=BehaviorType.directional,
+            samples=["This is a test", "This is a 2nd test"],
+            labels=[
+                [Span(start=2, end=5, label=10), Span(start=1, end=8, label=10)],
+                [Span(start=2, end=5, label=1), Span(start=1, end=8, label=0)]
+            ],
+            predict_fn=lambda x: [
+                [Span(start=2, end=5, label=6), Span(start=1, end=8, label=10)],
+                [Span(start=2, end=2, label=0), Span(start=1, end=8, label=0), Span(start=4, end=6, label=1)]
+            ]
+        )
+        performer = Performer(
+            metric_type="weighted",
+            binarize=True
+        )
+        performer.fit([span_classification_behavior, span_classification_behavior2])
+
+        assert performer.result[f"Behavior type - {BehaviorType.directional.value}"] == \
+               performer.result[f"Name - {span_classification_behavior2.name}"] == [0.5, '1/2']
+        assert performer.result[f"Behavior type - {BehaviorType.invariance.value}"] == \
+               performer.result[f"Name - {span_classification_behavior.name}"] == [1.0, '1/1']
+        assert performer.result["Total"] == [2 / 3, '2/3']
 
     def test_metrics_token_classification(self):
         """"""
@@ -100,7 +139,7 @@ class TestPerformer:
         performer.fit([token_classification_behavior, token_classification_behavior2])
 
         assert performer.result[f"Behavior type - {BehaviorType.directional.value}"] == \
-               performer.result[f"Name - {token_classification_behavior2.name}"] == 0.5
+               performer.result[f"Name - {token_classification_behavior2.name}"] == [0.5, '1/2']
         assert performer.result[f"Behavior type - {BehaviorType.invariance.value}"] == \
-               performer.result[f"Name - {token_classification_behavior.name}"] == 0
-        assert performer.result["Total"] == 1 / 3
+               performer.result[f"Name - {token_classification_behavior.name}"] == [0, '0/1']
+        assert performer.result["Total"] == [1 / 3, '1/3']
